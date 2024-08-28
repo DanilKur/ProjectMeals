6614e2e2aacae600af9d8bd9a0906e2cf90e697d
KDA <KDA@d.c>
2024-08-28 09:46:32 +0300

    AddHttp

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
     <header id="main-header">
       <div id="title">
         <img src={logoImg} alt="A restaurant" />
-        <h1>ReactFood</h1>
+        <h1>Arcobaleno</h1>
       </div>
       <nav>
         <Button textOnly onClick={handleShowCart}>
